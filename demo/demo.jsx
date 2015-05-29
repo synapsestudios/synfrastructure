9cf0a9376c58cd8b23681bf2886476ba4d113fbb
Kyle Cesmat <kyle@kylecesmat.com>
2015-05-28 17:07:12 -0700

    #21 | wrap demo container.

--- demo/demo.jsx
+++ demo/demo.jsx
@@ -1,15 +1,21 @@
 'use strict';
 
 var React  = require('react');
-var Alert  = require('../src/alert');
-var Button = require('../src/button');
+var Alert  = require('../dist/alert');
+var Button = require('../dist/button');
 
 module.exports = React.createClass({
 
     render : function()
     {
+        var styles = {
+            maxWidth : '720px',
+            margin   : '0 auto',
+            padding  : '20px'
+        }
+
         return (
-            <div>
+            <div style={styles}>
                 <h1>Alert</h1>
                 <Alert message='Alert'/>
                 <h1>Button</h1>
