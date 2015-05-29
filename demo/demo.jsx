3f594ef9b594f67a2f0adaafe5f19ebd20a23dc6
Kyle Cesmat <kyle@kylecesmat.com>
2015-05-29 09:03:53 -0700

    #21 | bump dependancys and update examples.

--- demo/demo.jsx
+++ demo/demo.jsx
@@ -3,6 +3,8 @@
 var React  = require('react');
 var Alert  = require('../dist/alert');
 var Button = require('../dist/button');
+var Input  = require('../dist/input');
+var Label  = require('../dist/label');
 
 module.exports = React.createClass({
 
@@ -16,11 +18,24 @@ module.exports = React.createClass({
 
         return (
             <div style={styles}>
-                <h1>Alert</h1>
-                <Alert message='Alert'/>
-                <h1>Button</h1>
-                <Button>Button</Button>
-                <h1>...and so on</h1>
+                <div>
+                    <h1>Alert Component</h1>
+                    <Alert message='This Is An Alert'/>
+                </div>
+                <div>
+                    <h1>Button Component</h1>
+                    <Button>This is a Button</Button>
+                </div>
+                <div>
+                    <h1>Input Component</h1>
+                    <Input id='example1' />
+                </div>
+                <div>
+                    <h1>Label Component</h1>
+                    <Label htmlFor='example2' text='This Is a Label'>
+                        <Input id='example2' placeholder='A label wraps around me' />
+                    </Label>
+                </div>
             </div>
         );
     }
