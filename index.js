6e6e2af10eb8f0aafbf0ef59e673d0c5b2118514
Nathan Stormer <nate@synapsestudios.com>
2015-06-01 10:56:04 -0700

    #26 | remove extra package exports.

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-var pkg  = require('./package.json');
-
-exports.libname = pkg.name;
-exports.path    = __dirname;
-exports.version = pkg.version;
-
 module.exports = {
     Alert  : require('./dist/alert'),
     Button : require('./dist/button'),
