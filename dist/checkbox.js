a596318b3392dddc5da79bc14295bd6c167919ff
Nathan Stormer <nate@synapsestudios.com>
2015-06-01 12:50:27 -0700

    #28 | add checkbox form component.

---
+++ dist/checkbox.js
@@ -0,0 +1,99 @@
+/* jshint globalstrict: true */
+'use strict';
+
+var React = require('react');
+
+module.exports = React.createClass({
+
+    displayName: 'Synfrastructure-Checkbox',
+
+    propTypes: {
+        id: React.PropTypes.string.isRequired,
+        name: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
+        disabled: React.PropTypes.bool,
+        componentCSSClassName: React.PropTypes.string,
+        checked: React.PropTypes.bool.isRequired,
+        value: React.PropTypes.any,
+        onFocus: React.PropTypes.func,
+        onBlur: React.PropTypes.func,
+        onChange: React.PropTypes.func,
+        onKeyDown: React.PropTypes.func,
+        onKeyUp: React.PropTypes.func,
+        onKeyPress: React.PropTypes.func,
+        className: React.PropTypes.string
+    },
+
+    getDefaultProps: function getDefaultProps() {
+        return {
+            name: null,
+            disabled: false,
+            componentCSSClassName: 'checkbox',
+            checked: false,
+            value: null,
+            onFocus: null,
+            onBlur: null,
+            onChange: null,
+            onKeyUp: null,
+            onKeyDown: null,
+            onKeyPress: null,
+            className: null
+        };
+    },
+
+    onChange: function onChange() {
+        if (this.props.onChange) {
+            this.props.onChange(this.props.checked, event);
+        }
+    },
+
+    onFocus: function onFocus() {
+        if (this.props.onFocus) {
+            this.props.onFocus(this.props.checked, event);
+        }
+    },
+
+    onBlur: function onBlur() {
+        if (this.props.onBlur) {
+            this.props.onBlur(this.props.checked, event);
+        }
+    },
+
+    onKeyUp: function onKeyUp() {
+        if (this.props.onKeyUp) {
+            this.props.onKeyUp(this.props.checked, event);
+        }
+    },
+
+    onKeyDown: function onKeyDown() {
+        if (this.props.onKeyDown) {
+            this.props.onKeyDown(this.props.checked, event);
+        }
+    },
+
+    onKeyPress: function onKeyPress() {
+        if (this.props.onKeyPress) {
+            this.props.onKeyPress(this.props.checked, event);
+        }
+    },
+
+    render: function render() {
+        var classes = [this.props.componentCSSClassName, this.props.className].join(' ');
+
+        return React.createElement('input', {
+            className: classes,
+            id: this.props.id,
+            name: this.props.name,
+            type: 'checkbox',
+            checked: this.props.checked,
+            value: this.props.value,
+            onFocus: this.onFocus,
+            onBlur: this.onBlur,
+            onChange: this.onChange,
+            onKeyUp: this.onKeyUp,
+            onKeyDown: this.onKeyDown,
+            onKeyPress: this.onKeyPress,
+            disabled: this.props.disabled
+        });
+    }
+
+});
