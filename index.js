398b73e599a1e4eb124fb89843d9b71fbc0d5fc0
Kyle Cesmat <kyle@kylecesmat.com>
2015-06-23 13:53:15 -0700

    #28 | add to index.js.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
-    Alert  : require('./dist/alert'),
-    Button : require('./dist/button'),
-    Input  : require('./dist/input'),
-    Label  : require('./dist/label')
+    Alert    : require('./dist/alert'),
+    Button   : require('./dist/button'),
+    Input    : require('./dist/input'),
+    Label    : require('./dist/label'),
+    Checkbox : require('./dist/checkbox')
 };
