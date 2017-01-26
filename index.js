951225c93e3de60bd717b741cfd59336aeada201
Chris Heninger <chrish@synapsestudios.com>
2017-01-26 11:17:19 -0700

    Fix index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
-  AbstractToggle: require('./dist/AbstractToggle'),
   Checkbox: require('./dist/Checkbox'),
   Label: require('./dist/Label'),
   Modal: require('./dist/Modal'),
   Radio: require('./dist/Radio'),
+  Toggle: require('./dist/Toggle'),
 };
